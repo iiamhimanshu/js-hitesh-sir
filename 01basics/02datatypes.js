48bda91bce50351aea0416e2e44a314b3036bb78
iiamhimanshu <140585825+iiamhimanshu@users.noreply.github.com>
2025-11-12 10:08:22 +0000

    datatypes

--- 01basics/02datatypes.js
+++ 01basics/02datatypes.js
@@ -1,3 +1,19 @@
-"use strict"; // treat all js code as newer version
+// "use strict"; // treat all js code as newer version
 
-alert(3 + 3);
+// alert(3 + 3) // we are using nodejs, not browser
+
+let age = 18;
+let isLoggedIn = false;
+let state = null;
+
+// number => 2 to power 53
+// bigint
+// String =>""
+// null => null is a standalone value=>null datatype is object
+// undefined => undefined datatype is undefined
+// Boolean => true/false
+// Symbol => unique
+
+console.log(typeof("himanshu")) //string
+console.log(typeof(undefined))  //undefined
+console.log(typeof(null))       //object
