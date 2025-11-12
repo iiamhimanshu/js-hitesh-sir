bc31e85f439f7bb03b236180dac44d7d80342db5
iiamhimanshu <140585825+iiamhimanshu@users.noreply.github.com>
2025-11-12 11:16:25 +0000

    operation

--- 01basics/03conversionOperation.js
+++ 01basics/03conversionOperation.js
@@ -27,3 +27,25 @@ let booleanisLoggedIn1 = Boolean(isLoggedIn1);
 console.log(booleanisLoggedIn1)
 
 // "" => false , "himanshu" => true
+
+
+// **************************  OPERATIONS  *********************************
+
+let str1 = "hello"
+let str2 = " himanshu"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2");
+console.log("1" + "2");
+
+// -------------------------------------------------------------
+
+console.log("1" + 2 + 2)
+// first waala string hai toh baaki sab ko wo string ki tarah treat kare ga
+console.log(1 + 2 + "2")
+// first waala number hai toh jab tak string na aa jaaye wo number ki tarah hi treat kare ga
+
+
+
