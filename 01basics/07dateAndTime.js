86220ca241c936238df3a7bd092ac87ce3436dde
iiamhimanshu <140585825+iiamhimanshu@users.noreply.github.com>
2025-11-13 08:15:27 +0000

    dateAndTime

---
+++ 01basics/07dateAndTime.js
@@ -0,0 +1,13 @@
+let myDate = new Date();
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(typeof(myDate));
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString())
+
+
+// let myCreatedDate = new Date(2025,10,13)
+let myCreatedDate = new Date(2025,10,13,5,40)
+console.log(myCreatedDate)
+console.log(myCreatedDate.toLocaleString())
