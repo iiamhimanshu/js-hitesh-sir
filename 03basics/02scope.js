d39e5b5351350ab0c8c8830dd8b9c1f2b89aa4be
iiamhimanshu <140585825+iiamhimanshu@users.noreply.github.com>
2025-11-14 08:52:28 +0000

    scope

---
+++ 03basics/02scope.js
@@ -0,0 +1,9 @@
+if(true){
+    let a = 10;
+    const b = 20;
+    var c = 30;
+}
+
+// console.log(a);       // ==> a scope se bahar hai
+// console.log(b);       // ==> b bhi scope se bahar hai
+console.log(c);          // ==> c run kar raha hai matlab var ka scope global hai
