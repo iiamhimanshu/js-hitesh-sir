28b3444a5d2df9979710f23c6d0e61153c09bb3e
iiamhimanshu <140585825+iiamhimanshu@users.noreply.github.com>
2025-11-15 08:59:36 +0000

    for loop

---
+++ 05iterations/one.js
@@ -0,0 +1,16 @@
+// for loop
+for(let i = 0; i<10; i++){
+    // console.log(i);
+    
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(`Outer loop value is ${i}`);
+
+    for (let j = 0; j < 10; j++) {
+        
+        console.log(`Outer loop value ${i} and inner loop value is ${j}`);        
+    }
+    
+    
+}
