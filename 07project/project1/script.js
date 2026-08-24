c453daa7fe8e47f450e490efc85861181852f385
iiamhimanshu <140585825+iiamhimanshu@users.noreply.github.com>
2026-08-24 16:06:28 +0000

    project 1 completed

--- 07project/project1/script.js
+++ 07project/project1/script.js
@@ -3,5 +3,10 @@ const buttons = document.querySelectorAll(".button");
 const body = document.querySelector("body");
 buttons.forEach(function(button){
     // console.log(button)
-    // 
+    button.addEventListener('click' , function(event){
+        // console.log(event);
+        // console.log(event.target);
+        const color = event.target.id;
+        body.style.backgroundColor = color ;
+    })
 })
