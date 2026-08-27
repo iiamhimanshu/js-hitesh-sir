8d891a911564e5b4db0cbfb5a6f4e5ae5ab3dedb
iiamhimanshu <140585825+iiamhimanshu@users.noreply.github.com>
2026-08-27 13:00:42 +0000

    project4 structure build

---
+++ 07project/project4/script.js
@@ -0,0 +1,64 @@
+let randomNo = parseInt(Math.random()*100 + 1);
+
+const guess = document.querySelector("#guessField");
+const submitBtn = document.querySelector("#subt");
+const prevGuess = document.querySelector(".guesses");
+const remGuess = document.querySelector(".lastResult");
+const msgDisplay = document.querySelector(".lowOrHi");
+const startOver = document.querySelector(".resultParas");
+
+const p = document.createElement("p");
+
+let prevGuessArr = [];
+let numGuess = 1;
+
+let playGame = true ;
+
+if(playGame){
+    submitBtn.addEventListener("click",function(e){
+        e.preventDefault();
+        const guessNo = parseInt(guess.value);
+        // console.log(guessNo);
+        validGuess(guessNo);
+    })
+}
+
+
+function validGuess(guessNo){
+    // here we are going to validate the guess
+    if(isNaN(guessNo)){
+        displayMsg(`Please enter a valid number`);
+    }else if(guessNo < 1){
+        displayMsg(`Number is less than 1`)
+    }else if(guessNo > 100){
+        displayMsg(`Number is greater than 100`)
+    }else{
+        checkGuess(guessNo);
+        prevGuessArr.push(guessNo);
+    }
+
+}
+
+function checkGuess(guessNo){
+    // here we are going to check the guess with that random no.
+
+}
+
+function displayGuess(guessNo){
+    // here we are going to display the guess in the prev guess array and also going to reset the guess and also going to update the remaing guesses count
+
+}
+
+function displayMsg(message){
+    // here we are going to display the message incase of various secenarios , such as when the no. is valid or not
+}
+
+function stopGame(){
+    // this function run when we have guessed the no. right or when we have or we have exauste the no. of lifeline given to us
+
+}
+
+function startGame(){
+    // this function run when we have to restart the game 
+}
+
